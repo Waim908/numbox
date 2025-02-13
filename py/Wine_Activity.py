c2818f57d04963c08531551904b048860733cde9
Waim908 <160860390+Waim908@users.noreply.github.com>
2025-02-13 15:44:06 +0800

    Update Wine_Activity.py

--- py/Wine_Activity.py
+++ py/Wine_Activity.py
@@ -1,9 +1,5 @@
-import subprocess
 import os
 import time
-startupinfo = subprocess.STARTUPINFO()
-startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
 os.startfile("wfm.exe")
-subprocess.Popen(["taskkill", "/f", "/im", "services.exe"], startupinfo=startupinfo)
 while True:
     time.sleep(65565)
