429197411e8f36f1cc3bcbcec13de539fcc28885
Waim908 <waimxj449@foxmail.com>
2025-03-02 15:42:25 +0800

    new

---
+++ script/Open-container.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source ~/NumBox/utils/path.conf
+source ~/NumBox/utils/file_list.sh ~/NumBox/container/ 容器 选择一个容器打开 "~/NumBox/container/"
+if [[ $lsterr == 2 ]]; then
+  dialog --title "╮(￣⊿￣)╭" --msgbox "容器不存在！请创建容器" $L $W && bash ~/NumBox/Numbox
+elif [[ $lsterr == 1 ]]; then
+  bash ~/NumBox/Numbox
+else
+  export CONTAINER_NAME=$BACK_NAME
+  bash ~/NumBox/startup-container.sh
+fi
---
+++ script/utils/file_list.sh
@@ -0,0 +1,20 @@
+# 声明路径>$1 ; 指定说明 标题$2 副标题$3 背景标题$4 ; 错误赋值$lsterr=1 取消选择 2 路径下无文件
+read L W H < ~/NumBox/custom-size
+FILE_COUNT=1
+MENU_OPTIONS=()
+# MENU_OPTIONS+=("0" "Back (返回)")
+while IFS= read -r file; do
+    filename=$(basename "$file")
+    MENU_OPTIONS+=("$FILE_COUNT" "$filename")
+    ((FILE_COUNT++))
+done < <(ls -A $1)
+if [ ${#MENU_OPTIONS[@]} -eq 0 ]; then
+    lsterr=2
+fi
+selection=$(dialog --cancel-label "返回" --title "$2" --backtitle "$4" --menu "$3" $L $W $H \
+       "${MENU_OPTIONS[@]}" 2>&1 >/dev/tty)
+if [ -z "$selection" ]; then
+    lsterr=1
+fi
+let index=($selection-1)*2+1
+BACK_NAME=${MENU_OPTIONS[$index]}
