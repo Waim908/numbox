a7ac0483698717a712e399e16e6e788cce922833
Waim908 <waimxj449@foxmail.com>
2025-04-15 17:16:21 +0800

    update script file_list.sh

--- NumBox/utils/file_list.sh
+++ NumBox/utils/file_list.sh
@@ -1,5 +1,6 @@
-# 声明路径>$1 ; 指定说明 标题$2 副标题$3 背景标题$4 ; 错误赋值$lsterr=1 取消选择 2 路径下无文件 ; 排序类型 $list_type
-# 需要utils/dialog.sh生效
+# 声明路径>$1 ; 指定说明 标题$2 副标题$3 背景标题$4 ; 错误赋值$?=1 排序类型 $list_type
+# 需要utils/dialog.conf生效
+if [ "$(ls -A $1)" ]; then
 if [[ -z $list_type ]]; then
   list_type=A
 fi
@@ -14,10 +15,17 @@ done < <(ls -$list_type $1)
 if [ ${#MENU_OPTIONS[@]} -eq 0 ]; then
     lsterr=2
 fi
-selection=$(dialog "${dialog_arg[@]}" --title "$2" --backtitle "$4" --menu "$3" 0 -1 0 \
+selection=$(dialog $arg --no-cancel --title "$2" --backtitle "$4" --menu "$3" $box_sz \
        "${MENU_OPTIONS[@]}" 2>&1 >/dev/tty)
-if [ -z "$selection" ]; then
-    lsterr=1
-fi
 let index=($selection-1)*2+1
 BACK_NAME=${MENU_OPTIONS[$index]}
+else
+  ls $1 2>/dev/null
+  if [[ $? == 0 ]]; then
+    dir_is=文件路径下无文件
+  else
+    dir_is=文件路径不存在
+  fi
+  dialog ${dialog_arg[@]} --title "$dir_is" --msgbox "$1" $box_sz2
+  return 1
+fi
